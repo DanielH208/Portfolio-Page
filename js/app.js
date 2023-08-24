4423b1b6be7569ffa524e7a549773bee69f554e2
Daniel Higgins <daniel.higgins7@btinternet.com>
2023-08-24 14:57:27 +0100

    Added typewriter animation for banner text

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$( "#about-me-content-container" ).click(function() {
+    alert( "I've been clicked!" );
+  });
+
+
+$("#about-me-info").typewriter({
+    waitingTime: 0,
+    delay: 70,
+    hide: 0,
+    cursor:true,
+
+});
