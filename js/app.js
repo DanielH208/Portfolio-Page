ab78cb640627e61b2ceb34f518c8c6de124eef8d
Daniel Higgins <daniel.higgins7@btinternet.com>
2023-08-25 10:25:26 +0100

    Added burger menu functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $( "#about-me-content-container" ).click(function() {
-    alert( "I've been clicked!" );
+    alert( "aaaaaaaaaaaaaaaaaa" );
   });
 
 
@@ -10,3 +10,57 @@ $("#about-me-info").typewriter({
     cursor:true,
 
 });
+
+// $("#about-me button").on("click", () => {
+//     $(".sidenav").show();
+//     $("#about-me button").addClass("hamburger hamburger--slider is-active");
+//     $("#about-me button").css("margin-left", "130px");
+// })
+// $("#about-me button").off("click", () => {
+//     $(".sidenav").hide();
+//     $("#about-me button").removeClass("hamburger hamburger--slider is-active");
+//     $("#about-me button").css("margin-left", "0px")
+// })
+
+let switchStatus = false;  
+$("#about-me button").on('click', function() {  
+    if (switchStatus == false) {
+        $(".sidenav").show();
+        $("#about-me button").addClass("hamburger hamburger--slider is-active");
+        $("#about-me button").css("margin-left", "130px");
+        $("main").css("filter", "brightness(50%)")
+        $("footer").css("filter", "brightness(50%)")
+        switchStatus = true;
+    }
+    else {
+        $(".sidenav").hide();
+        $("#about-me button").removeClass("hamburger hamburger--slider is-active");
+        $("#about-me button").css("position", "absolute");
+        $("#about-me button").css("padding", "15px");
+        $("#about-me button").css("margin-left", "0px");
+        $("main").css("filter", "brightness(100%)")
+        $("footer").css("filter", "brightness(100%)")
+        switchStatus = false;
+    }
+});
+
+
+// $("#about-me button").click(() => {
+    
+//     $(".sidenav").toggle();
+//     // $("#about-me button").slideToggle(() => {
+//     //     $("#about-me button").addClass("hamburger hamburger--slider is-active");
+//     // });
+
+    
+
+
+//     // $("#about-me button").css("margin-left", "130px");
+//     // $(".sidenav").toggle(() => {
+//     //     $("#about-me button").css("margin-left", "0px");
+        
+//     // });
+//     // $("#about-me button").css("margin-left", "0px");
+    
+// }).addClass("hamburger hamburger--slider is-active");
+
